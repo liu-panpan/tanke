3de01d1b08a2909446074eb78dbf385af4281491
liu-panpan <809312009@qq.com>
2021-08-07 16:48:26 +0800

    学习使用SimpleChannelInboundHandler

--- src/main/java/com/panpan/tank/net/Client.java
+++ src/main/java/com/panpan/tank/net/Client.java
@@ -9,9 +9,9 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -87,10 +87,10 @@ class ClientChannelInitializer extends ChannelInitializer<SocketChannel> {
 
 }
 
-class ClientHandler extends ChannelInboundHandlerAdapter {
+class ClientHandler extends SimpleChannelInboundHandler<TankJoinMsg> {
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead0(ChannelHandlerContext ctx, TankJoinMsg msg) throws Exception {
         System.out.println(msg);
     }
 
